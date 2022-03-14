7f67b61131509bb61360a37e8f749f182b63e29a
Windows <Windows@Windows-PC>
2022-03-14 12:53:04 +0530

    New commit

--- src/test/java/demoGit/DemoTest.java
+++ src/test/java/demoGit/DemoTest.java
@@ -2,15 +2,22 @@ package demoGit;
 
 import org.testng.annotations.Test;
 
-public class Demo {
+public class DemoTest {
 	@Test
-	public void demo()
+	public void createOrg()
 	{
 		System.out.println("User1 - First commit");
 		
 		
 		
 		System.out.println("User2 --- ssecond commit");
+		
+		System.out.println("Merge user3");
+	}
+	
+	public void createContact()
+	{
+		System.out.println("create Org");
 	}
 
 }
